e47876e6d8faf99588d78d20d76ae58ab46ae6aa
Ethan Armstrong <50377734+ethanarm123@users.noreply.github.com>
2024-11-17 12:27:13 -0500

    Upload .sql file

---
+++ t177.db.sql
@@ -0,0 +1,21 @@
+SELECT title, due_date
+FROM assignments
+WHERE course_id = 'COMP1234';
+
+SELECT MIN(due_date) AS earliest_due_date
+FROM assignments;
+
+SELECT MAX(due_date) AS latest_due_date
+FROM assignments;
+
+SELECT title, course_id
+FROM assignments
+WHERE due_date = '2024-10-08';
+
+SELECT title, due_date
+FROM assignments
+WHERE due_date LIKE '2024-10%';
+
+SELECT MAX(due_date) AS most_recent_completed_due_date
+FROM assignments
+WHERE status = 'Completed';
